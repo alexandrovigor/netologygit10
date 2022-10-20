c428140a9623618feaaff8f56464ed201b9a5c3a
alexandrovigor <gsandy67@mail.ru>
2022-10-20 14:48:24 +0300

    Add average counter

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,26 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int averageValue() {
+        int sum = 0;
+        int i = -2;
+        for (int sale : sales) {
+            if (sale != min() && sale != max())
+                sum += sale;
+            i++;
+        }
+        return (sum / i);
+    }
 }
+
