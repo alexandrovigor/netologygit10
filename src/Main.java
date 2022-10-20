3a5e1f71029042e61f91828e44aa5ae57b63575f
alexandrovigor <gsandy67@mail.ru>
2022-10-20 14:20:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] sale = {10, 20, 30, 16, 2, 8};
+        SalesManager sales = new SalesManager(sale);
+        System.out.println("Max sale: " + sales.max());
+    }
+}
